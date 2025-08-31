c09b5d57c4ff0594f78261d8f67fbadab4f79d71
xavier72bit <Xavier72bit@163.com>
2025-08-31 16:53:03 +0800

    后端代码init

---
+++ bttn-backend/src/main/java/dev/xavier72bit/bttn/BlockTravellerTestingNexusApplication.java
@@ -0,0 +1,13 @@
+package dev.xavier72bit.bttn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockTravellerTestingNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockTravellerTestingNexusApplication.class, args);
+	}
+
+}
---
+++ bttn-backend/src/test/java/dev/xavier72bit/bttn/BlockTravellerTestingNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xavier72bit.bttn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockTravellerTestingNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
